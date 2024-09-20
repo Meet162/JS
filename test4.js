dc1476ed95271c35ac8aca2c09949ab1e90077e0
Meet162 <meetrana878@gmail.com>
2024-09-20 17:19:19 +0530

    Done

---
+++ test4.js
@@ -0,0 +1,11 @@
+// console.log({
+//     'email': 'meetrana878@gmail.com',
+//     "groups": {},
+//     'id': 33,
+//     'title': 'test'
+// });
+// console.log(document.body);
+
+prompt(text);
+let age = prompt("avarage age");
+console.log(age);
